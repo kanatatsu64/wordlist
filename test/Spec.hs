664ca12faaab62e6a7b1b60a0c155da08d0c0286
kanatatsu64 <kanatatsu64@gmail.com>
2020-11-28 06:29:57 +0900

    fix test root

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Test.Tasty
 import qualified CsvTest
 import qualified HtmlTest
 import qualified BundleTest
+import qualified DirectoryTest
 import qualified UtilsTest
 import qualified German.Spec as GermanTest
 
@@ -10,6 +11,7 @@ test_all = testGroup "main" [
         test_csv,
         test_html,
         test_bundle,
+        test_directory,
         test_utils,
         test_german
     ]
@@ -17,8 +19,9 @@ test_all = testGroup "main" [
 test_csv = CsvTest.test_all
 test_html = HtmlTest.test_all
 test_bundle = BundleTest.test_all
+test_directory = DirectoryTest.test_all
 test_utils = UtilsTest.test_all
 test_german = GermanTest.test_all
 
 main :: IO ()
-main = defaultMain test_utils
+main = defaultMain test_all
