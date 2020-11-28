5e5b60ec0909c1704c1f9b004f34e7e7701debf3
kanatatsu64 <kanatatsu64@gmail.com>
2020-11-28 12:25:28 +0900

    add build settings

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: {
+        top: './src/index.tsx',
+        csv: './src/Csv/index.tsx'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.tsx|\.ts)$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+        modules: [ path.resolve(__dirname, 'src'), 'node_modules' ]
+    }
 };
