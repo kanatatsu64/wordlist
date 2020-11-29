52fea86fb9f9e6273af0718ced43c0430b40d75a
kanatatsu64 <kanatatsu64@gmail.com>
2020-11-29 12:31:37 +0900

    fix webpack settings

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: ['**/*', '!.gitkeep']
+            cleanOnceBeforeBuildPatterns: ['**/*', '!.gitignore', '!.gitkeep']
         }),
         new HtmlWebpackPlugin({
             chunks: ['top'],
