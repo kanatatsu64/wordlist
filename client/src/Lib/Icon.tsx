6b00eec94f89146b15f5d732c1ef3423fe409af1
kanatatsu64 <kanatatsu64@gmail.com>
2020-11-30 22:11:55 +0900

    add Icon

---
+++ client/src/Lib/Icon.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import style from './IconStyle.scss'
+
+type DeletePropsType = {
+    onClick: () => void
+}
+
+export const Delete: React.FC<DeletePropsType> = props => {
+    const onClick = (event: React.MouseEvent) => {
+        event.preventDefault()
+        props.onClick()
+    }
+
+    const className = [
+        'material-icons',
+        style.i
+    ].join(' ')
+
+    return (
+        <div className={ style.delete }>
+            <i className={ className } onClick={ onClick }>
+                clear
+            </i>
+        </div>
+    )
+}
