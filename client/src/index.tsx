d411662af147206b0a8e3c3ee5c35622d0670aa5
kanatatsu64 <kanatatsu64@gmail.com>
2020-11-30 22:47:33 +0900

    use Home Logo

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import { Home } from 'Logo'
 import { TopPage } from 'Pages/TopPage'
 import { CsvTablePage } from 'Pages/CsvTablePage'
 import { CardPage } from 'Pages/CardPage'
 
 const App = (
     <Router>
+        <Home></Home>
         <Switch>
             <Route exact path="/">
                 <TopPage></TopPage>
