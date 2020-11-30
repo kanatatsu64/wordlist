b3b4adc82b6ef9f537b8b8738453db3ed403cfa9
kanatatsu64 <kanatatsu64@gmail.com>
2020-12-01 00:11:03 +0900

    add webpack persistent cache
    There is currently an issue to note, related to webpack config.
    https://github.com/webpack/webpack-cli/issues/1924

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,7 +1,15 @@
+const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.config')
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    cache: {
+        type: 'filesystem',
+        cacheDirectory: path.resolve(__dirname, 'cache'),
+        buildDependencies: {
+            config: [__filename]
+        }
+    }
 })
