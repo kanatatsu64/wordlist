7784577d41a666e7274994bd57ed33d6a65b57b3
kanatatsu64 <kanatatsu64@gmail.com>
2020-12-02 06:11:21 +0900

    make German Plugin

---
+++ src/Plugins/German/Adjective.hs
@@ -0,0 +1,56 @@
+module German.Adjective (
+    parse,
+    toHtml,
+
+    Comparative (..),
+    Superlative (..),
+    parseAttrs
+) where
+
+import Prelude hiding ( word )
+
+import Serializable ( Serializable (..) )
+import Html ( Html (..), TagName (..), Content (..) )
+import German.Card ( Card (..), Attr (..) )
+import German.Utils ( parseWord, parseMeaning, parseNote, parseExamples, (>:>) )
+
+newtype Comparative = Comparative String
+
+instance Serializable Comparative where
+    serialize (Comparative comp) = comp
+
+newtype Superlative = Superlative String
+
+instance Serializable Superlative where
+    serialize (Superlative sup) = sup
+
+parse = parseWord >:>
+        parseAttrs >:>
+        parseMeaning >:>
+        parseNote >:>
+        parseExamples
+
+parseAttrs = parseComparative >:> parseSuperlative
+
+parseComparative (comp:rests) cons next = next rests (_cons (Comparative comp))
+    where _cons comp sup = cons [Attr comp, Attr sup]
+
+parseSuperlative (sup:rests) cons next = next rests (cons (Superlative sup))
+
+{-
+    Card German Adjective klein [-er, -est] small _ [ein kleines Kind, a small child]
+
+    <tr>
+        <td> klein </td>
+        <td> Adj. small </td> <--! dismiss comparative & superlative -->
+        <--! dismiss examples -->
+    </tr>
+-}
+
+toHtml card = Tag TR [] [Child $ firstTd card, Child $ secondTd card]
+    where firstTd card = Tag TD [] [Text $ word card]
+          secondTd card = Tag TD [] [Text $ serialize (part card) ++ " " ++ meaning card ++ _note]
+          _note = case note card of
+            "" -> ""
+            _ -> " (" ++ note card ++ ")"
+
---
+++ src/Plugins/German/Base.hs
@@ -0,0 +1,11 @@
+module German.Base (
+    Card (..),
+    Part (..),
+    Genre (..),
+    Kind (..) 
+) where
+
+import German.Proc ()
+import German.Card ( Card (..), Part (..) )
+import German.Noun ( Genre (..) )
+import German.Verb ( Kind (..) )
