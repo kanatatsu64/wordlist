b7f0c4fc81bca728b1a9287b48c597ac57fe85ad
kanatatsu64 <kanatatsu64@gmail.com>
2020-12-05 15:16:36 +0900

    add Dial

---
+++ client/src/Lib/Dial.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+type PropsType = {
+    onNext: () => void
+    onPrev: () => void
+    interval?: number
+}
+
+export const Dial: React.FC<PropsType> = props => {
+    const interval = props.interval || 100
+
+    const [lock, setLock] = React.useState(false)
+
+    const onWheel = (event: React.WheelEvent) => {
+        if (lock) return
+
+        setLock(true)
+        const delta = event.deltaY
+        if (delta > 0) {
+            props.onNext()
+        } else if (delta < 0) {
+            props.onPrev()
+        }
+        setTimeout(() => setLock(false), interval)
+    }
+
+    return (
+        <div onWheel={ onWheel }>
+            { props.children }
+        </div>
+    )
+}
