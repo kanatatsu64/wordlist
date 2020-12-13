61c29655372786cf6b2dc9c626b920353b8a78b9
kanatatsu64 <kanatatsu64@gmail.com>
2020-12-13 23:50:59 +0900

    add Bundle and Card Apis

--- client/src/Api/Bundle.ts
+++ client/src/Api/Bundle.ts
@@ -1,18 +1,34 @@
 import axios, { AxiosResponse } from 'axios'
 
-import { Bundle, BundleID, BundleInfo, PluginID } from 'Types'
+import { Bundle, BundleID, BundleInfo, CardID, PluginID, PseudoID } from 'Types'
+import * as CardApi from 'Api/Card'
 import { BaseApiURL } from 'Api/Config'
 
 const BundleApiURL = BaseApiURL + 'bundle/'
 
 export const create = async (name: string, desc: string) => {
-    await axios.post(BundleApiURL, null, { params: { name, desc } })
+    const bundleid = PseudoID
+    const cardids = []
+    await axios.post(BundleApiURL, { bundleid, name, desc, cardids })
 }
 
 export const upload = async (bundleId: BundleID, file: File, pluginId: PluginID) => {
-    const data = new FormData()
-    data.append("csv", file)
-    await axios.post(BundleApiURL + bundleId, data, { params: { plugin: pluginId } })
+    const cardIds = await CardApi.upload(file, pluginId)
+    await addCards(bundleId, cardIds)
+}
+
+export const addCards = async (bundleId: BundleID, cardIds: CardID[]) => {
+    await axios.post(BundleApiURL + bundleId + '/cards', cardIds)
+}
+
+export const deleteCards = async (bundleId: BundleID, cardIds: CardID[]) => {
+    await axios.delete(BundleApiURL + bundleId + '/cards', { data: cardIds })
+}
+
+export const update = async (bundle: Bundle) => {
+    const { bundleid, name, desc, cards } = bundle
+    const cardids = cards.map(card => card.cardid)
+    await axios.put(BundleApiURL, { bundleid, name, desc, cardids })
 }
 
 export const load = async (bundleId: BundleID) => {
@@ -25,4 +41,8 @@ export const loadInfos = async () => {
     type DataType = BundleInfo[]
     const res: AxiosResponse<DataType> = await axios.get(BundleApiURL + 'list/name')
     return res.data
+}
+
+export const delete_ = async (bundleId: BundleID) => {
+    await axios.delete(BundleApiURL + bundleId)
 }
--- client/src/Api/Card.ts
+++ client/src/Api/Card.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 
-import { CardID, Card } from 'Types'
+import { CardID, Card, PluginID } from 'Types'
 import { BaseApiURL } from 'Api/Config'
 
 const BaseCardURL = BaseApiURL + 'card/'
@@ -9,4 +9,20 @@ export const load = async (cardId: CardID) => {
     type DataType = Card
     const res: AxiosResponse<DataType> = await axios.get(BaseCardURL + cardId)
     return res.data
+}
+
+export const upload = async (file: File, pluginId: PluginID) => {
+    type DataType = CardID[]
+    const data = new FormData()
+    data.append("csv", file)
+    const res: AxiosResponse<DataType> = await axios.post(BaseCardURL + 'list', data, { params: { plugin: pluginId } })
+    return res.data
+}
+
+export const update = async (card: Card) => {
+    await axios.put(BaseCardURL, card)
+}
+
+export const delete_ = async (cardId: CardID) => {
+    await axios.delete(BaseCardURL + cardId)
 }
